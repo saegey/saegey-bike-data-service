b828ad78acd427dc6ee7ba91cba77aeeca75b86d
Adam Saegebarth <adams@synapse.com>
2014-05-07 23:23:09 +0000

    add passport github package

--- app.js
+++ app.js
@@ -33,38 +33,38 @@ db.once("open", function callback() {
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
 
 
-// Use the LocalStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
-//   with a user object.  In the real world, this would query a database;
-//   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(function(username, password, done) {
-  User.findOne({ username: username }, function(err, user) {
-    if (err) { return done(err); }
-    if (!user) {
-      return done(null, false, { message: "Unknown user " + username });
-    }
-    user.comparePassword(password, function(err, isMatch) {
-      if (err) return done(err);
-      if(isMatch) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: 'Invalid password' });
-      }
-    });
-  });
-}));
+// // Use the LocalStrategy within Passport.
+// //   Strategies in passport require a `verify` function, which accept
+// //   credentials (in this case, a username and password), and invoke a callback
+// //   with a user object.  In the real world, this would query a database;
+// //   however, in this example we are using a baked-in set of users.
+// passport.use(new LocalStrategy(function(username, password, done) {
+//   User.findOne({ username: username }, function(err, user) {
+//     if (err) { return done(err); }
+//     if (!user) {
+//       return done(null, false, { message: "Unknown user " + username });
+//     }
+//     user.comparePassword(password, function(err, isMatch) {
+//       if (err) return done(err);
+//       if(isMatch) {
+//         return done(null, user);
+//       } else {
+//         return done(null, false, { message: 'Invalid password' });
+//       }
+//     });
+//   });
+// }));
 
 
 var app = express();
@@ -80,10 +80,6 @@ app.configure(function () {
   app.use(express.methodOverride());
   app.use(logfmt.requestLogger());
   app.use(express.session({ secret: 'keyboard cat' }));
-  // app.use(function(req, res, next) {
-  //    res.locals.user = req.user;
-  //    next();
-  // })
 
   app.use(function(req, res, next){
     res.locals.moment = require('moment')
