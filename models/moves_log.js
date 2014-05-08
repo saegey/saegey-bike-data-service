d070d9b7ac7fd1f210de3097b85506e780657c6d
Adam Saegebarth <adams@synapse.com>
2014-05-08 07:04:05 +0000

    delete unused moves log

--- models/moves_log.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var movesLogSchema = mongoose.Schema({
-  apiEndpoint: { type: String, required: true },
-  numOfRecords: { type: Number, required: true },
-  updated: { type: Date, default: Date.now }
-});
-var MovesLog = mongoose.model('movesLog', movesLogSchema);
-module.exports = MovesLog;
