f5d85aebd9764e648b910e3797508cda7a86542d
Adam Saegebarth <adams@synapse.com>
2014-05-08 07:04:49 +0000

    remove local passport auth

--- models/user.js
+++ models/user.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
 
 // User Schema
 var userSchema = mongoose.Schema({
@@ -9,32 +7,5 @@ var userSchema = mongoose.Schema({
   displayName: { type: String}
 });
 
-// Bcrypt middleware
-// userSchema.pre('save', function(next) {
-//   var user = this;
-
-//   if (!user.isModified('password')) {
-//     return next();
-//   }
-
-//   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//     if(err) return next(err);
-
-//     bcrypt.hash(user.password, salt, function(err, hash) {
-//       if(err) return next(err);
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-// Password verification
-// userSchema.methods.comparePassword = function(candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//     if(err) return cb(err);
-//     cb(null, isMatch);
-//   });
-// };
-
-User = mongoose.model('user', userSchema);
+var User = mongoose.model('user', userSchema);
 module.exports = User;
