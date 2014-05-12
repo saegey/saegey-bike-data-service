92a936c112dec02584104e674294bc10708645e0
Adam Saegebarth <adams@synapse.com>
2014-05-12 10:11:55 -0700

    move bower_components directory so its excluded by linguist

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.masthead-nav li').each(function() {
-    console.log(window.location.pathname);
-    if ($("a", this).attr('href')  ===  window.location.pathname) {
-      $(this).addClass('active');
-    }
-  });
-})
