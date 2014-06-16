a8a4d7d433e6924d673cefc96fdd2ac05876d3d4
Adam Saegebarth <adams@synapse.com>
2014-06-15 17:49:04 -0700

    Schema changes to instagram photo

--- models/instagram_photo.js
+++ models/instagram_photo.js
@@ -9,7 +9,12 @@ var instagramPhotoSchema = mongoose.Schema({
     id: { type: String, required: true },
     attribution: { type: String, required: false },
     tags: { type: String, required: false },
-    location: { type: String, required: false },
+    location:  {
+      latitude: { type: Number },
+      name: { type: String },
+      longitude: {type: Number },
+      id: { type: Number }
+    },
     filter: { type: String, required: true },
     created_time: { type: Date, required: true },
     link: { type: String, required: true },
